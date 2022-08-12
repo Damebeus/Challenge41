5e17ab223d61fb6e83cfebd157d8ce7aa03e984b
damebeus <damiebeus@gmail.com>
2022-08-12 15:18:12 -0300

    boton landing

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import inferior from "./inferior.png";
 import style from "./Welcome.module.css";
+import { Link } from "react-router-dom";
+
 const Welcome = () => {
   return (
-    <div className={style.container}>
-      <div className={style.titulo}>
-        <h1>Welcome</h1>
-      </div>
-      <div className={style.subtitulo}>
-        <h2>Search your favorite crypto currency</h2>
-      </div>
+    <div className={style.global}>
+      <div className={style.container}>
+        <div className={style.titulo}>
+          <h1>Welcome</h1>
+        </div>
+        <div className={style.subtitulo}>
+          <h2>Search your favorite crypto currency</h2>
+        </div>
 
-      <div className={style.imagen}>
-        <img src={inferior} alt='' />
+        <div className={style.imagen}>
+          <img src={inferior} alt='' />
+        </div>
+        <div className={style.boton}>
+          <Link to='/home'>
+            <button>Lets go!</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
