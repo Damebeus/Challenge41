8b670fe228b33b1408c22cdb49c468ddcf624ba7
damebeus <damiebeus@gmail.com>
2022-08-12 15:48:54 -0300

    navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 import style from "./Home.module.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={style.container}>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./Navbar.module.css";
+const Navbar = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.titulo}>CryptoFav</div>
+    </div>
+  );
+};
+
+export default Navbar;
