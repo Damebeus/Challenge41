f34cfab6a0c95c173cb0d09506458d693587d49c
damebeus <damiebeus@gmail.com>
2022-08-13 13:41:14 -0300

    search page remaquetado

--- src/components/Searchpage/Searchpage.jsx
+++ src/components/Searchpage/Searchpage.jsx
@@ -12,19 +12,32 @@ const Searchpage = () => {
         <Searchbar />
       </div>
       <div className={style.monedas}>
+        <div className={style.subtitulo}>
+          <p>Nombre</p>
+          <p>Precio</p>
+        </div>
         <Link to='/favoritos'>
           <div className={style.detalles}>
             <h2>BTC</h2>
+            <div className={style.precio}>
+              <h3>$24,537</h3>
+            </div>
           </div>
         </Link>
         <Link to='/favoritos'>
           <div className={style.detalles}>
             <h2>DAI</h2>
+            <div className={style.precio}>
+              <h3>$327.7</h3>
+            </div>
           </div>
         </Link>
         <Link to='/favoritos'>
           <div className={style.detalles}>
             <h2>ETH</h2>
+            <div className={style.precio}>
+              <h3>$2,402</h3>
+            </div>
           </div>
         </Link>
       </div>
