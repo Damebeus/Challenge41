c5f5fb23dad4f06c309224101fdbf7c31bfe16f1
damebeus <damiebeus@gmail.com>
2022-08-13 11:19:27 -0300

    botones maquetados

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,37 @@ import React from "react";
 import Navbar from "../Navbar/UnarrowNavBar";
 import style from "./Home.module.css";
 import { MdOutlineAddCircleOutline } from "react-icons/md";
+import { RiCloseCircleLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className={style.container}>
       <Navbar />
+
+      <div className={style.monedas}>
+        <div className={style.detalles}>
+          <h2>BTC</h2>
+          <div className={style.cantidad}>
+            <h3>$20.508</h3>
+          </div>
+          <RiCloseCircleLine className={style.quitar} />
+        </div>
+        <div className={style.detalles}>
+          <h2>ETH</h2>
+          <div className={style.cantidad}>
+            <h3>$7.508</h3>
+          </div>
+          <RiCloseCircleLine className={style.quitar} />
+        </div>
+        <div className={style.detalles}>
+          <h2>DAO</h2>
+          <div className={style.cantidad}>
+            <h3>$508</h3>
+          </div>
+          <RiCloseCircleLine className={style.quitar} />
+        </div>
+      </div>
+
       <div className={style.agregar}>
         <Link to='/añadir'>
           <button>
